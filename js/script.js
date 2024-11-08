611d724bf6775b3c2046a3bca242ff18fd88ccf9
MauAnalyst <164758301+MauAnalyst@users.noreply.github.com>
2024-11-08 18:19:52 -0300

    add menu response

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+//---- menu responsivo
+const menu = document.querySelector("#menu-resp #icon");
+const menuItens = document.querySelector("#items-menu-resp");
+const backgroundMenu = document.querySelector("#background-menu-resp");
+const mediaQuery = window.matchMedia("(max-width: 700px)");
+
+menu.addEventListener("click", () => {
+  if (menu.textContent === "menu") {
+    menu.textContent = "close";
+    menuItens.style.display = "block";
+    backgroundMenu.style.display = "block";
+  } else {
+    menu.textContent = "menu";
+    menuItens.style.display = "none";
+    backgroundMenu.style.display = "none";
+  }
+});
+
+function handleScreenChange(e) {
+  if (!e.matches) {
+    menu.textContent = "menu";
+    menuItens.style.display = "none";
+    backgroundMenu.style.display = "none";
+  }
+}
+
+handleScreenChange(mediaQuery);
+
+mediaQuery.addEventListener("change", handleScreenChange);
+
+//---- adicionar usuário
+
 const userNameLink = document.querySelector("#userNameLink");
 const fastLink = document.querySelector(".user-link-input");
 
