c848a1133a2b5e4c3223d356569058c2c0a09d55
MauAnalyst <164758301+MauAnalyst@users.noreply.github.com>
2024-11-08 18:45:46 -0300

    ajust menu

--- js/script.js
+++ js/script.js
@@ -1,33 +1,41 @@
 //---- menu responsivo
 const menu = document.querySelector("#menu-resp #icon");
 const menuItens = document.querySelector("#items-menu-resp");
-const menuItem = document.querySelector("#items-menu-resp ul li");
+const menuItem = document.querySelectorAll("#items-menu-resp ul li");
 const backgroundMenu = document.querySelector("#background-menu-resp");
 const mediaQuery = window.matchMedia("(max-width: 700px)");
 
 menu.addEventListener("click", () => {
   if (menu.textContent === "menu") {
-    menu.textContent = "close";
-    menuItens.style.display = "block";
-    backgroundMenu.style.display = "block";
+    enableMenu();
   } else {
-    menu.textContent = "menu";
-    menuItens.style.display = "none";
-    backgroundMenu.style.display = "none";
+    disableMenu();
   }
 });
 
 backgroundMenu.addEventListener("click", () => {
-  menu.textContent = "menu";
-  menuItens.style.display = "none";
-  backgroundMenu.style.display = "none";
+  disableMenu();
 });
 
-menuItem.addEventListener("click", () => {
+menuItem.forEach((e) => {
+  e.addEventListener("click", () => {
+    disableMenu();
+  });
+});
+
+function disableMenu() {
   menu.textContent = "menu";
   menuItens.style.display = "none";
   backgroundMenu.style.display = "none";
-});
+  return;
+}
+
+function enableMenu() {
+  menu.textContent = "close";
+  menuItens.style.display = "block";
+  backgroundMenu.style.display = "block";
+  return;
+}
 
 function handleScreenChange(e) {
   if (!e.matches) {
@@ -35,6 +43,7 @@ function handleScreenChange(e) {
     menuItens.style.display = "none";
     backgroundMenu.style.display = "none";
   }
+  return;
 }
 
 handleScreenChange(mediaQuery);
